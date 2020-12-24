c1fa9079e7519eccc5661e97adad255a2276768b
Omer Gamliel <omergamliel3@gmail.com>
2020-12-24 17:01:44 +0200

    rename folder

---
+++ final exercise/my_math.py
@@ -0,0 +1,19 @@
+def add(a=0, b=0):
+    """ calculate the sum of a and b.
+        :param a: first argument (default = 0)
+        :type a: int
+        :param b: second argument (default = 0)
+        :type b: int
+        :rtype: int
+    """
+    return a + b
+
+
+def main():
+    # print black space and call the function add
+    print('\n')
+    print(add(2, 2))
+
+
+if __name__ == '__main__':
+    main()
